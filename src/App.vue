b6931bf029c1d4b91d25d8d0a6ee44ed1d3616b7
Albert Valdes <albert.anthony1999@gmail.com>
2021-05-21 11:18:23 -0500

    prettier/eslint working

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   },
   data() {
     return {
-      st: "sad",
+      st: 'sad',
     };
   },
-}
+};
 </script>
 
 <style>
