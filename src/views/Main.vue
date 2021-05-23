11cfbb9ac133e52a5386abe40a993ecbc544176b
Albert Valdes <albert.anthony1999@gmail.com>
2021-05-23 01:28:52 -0500

    Custom button created

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,7 +5,11 @@
       <div class="middle" />
       <div class="right" />
     </div>
-    <h1>Hello</h1>
+    <div class="contact-button button">
+      <div>Contact Me</div>
+      <div>Dev for hire!</div>
+    </div>
+    <img src="../assets/icons/hello.svg" alt="Hello" class="hello" />
   </section>
 </template>
 
@@ -17,5 +21,25 @@ export default {
 
 <style scoped lang="scss">
 .main {
+  .hello {
+    width: 340px;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .contact-button {
+    width: 200px;
+    height: 55px;
+    overflow: hidden;
+    margin: 0 auto;
+    &:hover div {
+      transform: translateY(-140%);
+    }
+    div {
+      transition: all 0.2s ease;
+      margin-bottom: 10px;
+    }
+  }
 }
 </style>
